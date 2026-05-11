dbae6bad8b865dc67587f6d855b03097c2af8c8a
Sajidh Mohammad <sajidhseven@gmail.com>
2026-05-11 23:10:42 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import axios from 'axios'
+import { useEffect } from "react";
+
+
+
+const App = () => {
+
+  const [jokes,setJokes] = useState([]);
+
+  useEffect( () => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch( (error) => {
+      console.log(error)
+    })
+  })
+
+  return (
+    <>
+    <h1>Chai Aur Code Backendd</h1>
+    <p>Jokes : {jokes.length}</p>
+
+    {
+      jokes.map((joke,index) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+
+        </div>
+      ))
+    }
+
+    </>
+  )
+}
+
+export default App
